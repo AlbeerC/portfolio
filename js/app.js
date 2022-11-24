fce47dac83b55aa3ae2eadeb757612d7834974c6
Alberto Caminos <albeerc@gmail.com>
2022-11-24 18:53:30 -0300

    header functionalities

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
 //Menú hamburguesa
 const btnToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".nav-list");
+const linkList = document.querySelector(".nav-list");
+const links = document.querySelectorAll(".nav-list li a");
 
 btnToggle.addEventListener("click", () => {
-    links.classList.toggle("d-none");
-})
+    linkList.classList.toggle("d-none");
+});
+links.forEach( (link) => {
+  link.addEventListener("click", () => {
+    linkList.classList.add("d-none");
+  })
+});
+
+// Cuando se scrollea de sección a sección se agrega una clase en la que se vea en pantalla
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const menuLink = document.querySelector(`.nav-list a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        document.querySelector(".nav-list a.selected").classList.remove("selected");
+        menuLink.classList.add("selected");
+      }
+    });
+  },
+  { rootMargin: "-30% 0px -70% 0px" }
+);
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    nav-list.classList.remove("d-none");
+  });
+
+  const hash = link.getAttribute("href");
+  const target = document.querySelector(hash);
+  if (target) {
+    observer.observe(target);
+  }
+});
+
 
 
 // Cuando se hace scroll hacia abajo se le agrega una clase al header
